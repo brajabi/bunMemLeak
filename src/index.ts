99d0ba32200a76556732caa82abd1b365c4a730f
Behnam Rajabifard <b.rajabifard@gmail.com>
2025-03-09 12:01:33 +0330

    First test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/download", () => {
+    // public
+    const filePath = process.cwd() + "/public/RandomFile.zip";
+    const randomFile = Bun.file(filePath);
+    // const fileBuffer = await randomFile.arrayBuffer()
+
+    // Return subtitle file as response
+    return new Response(randomFile.stream(), {
+      headers: {
+        "Content-Type": "application/octet-stream",
+        "Content-Disposition": `attachment; filename="RandomFile.zip"`,
+        "Content-Length": randomFile.size.toString(),
+      },
+    });
+  })
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
